0beb490496a22044d2d467321824a41dc009a492
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-25 15:40:51 +0900

    reactive-angular/Section5-33 1つのコンポーネントで一覧・詳細を実装。

--- reactive-angular/client/src/app/lesson/lesson.component.ts
+++ reactive-angular/client/src/app/lesson/lesson.component.ts
@@ -1,8 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Lesson } from '../model/lesson';
 
 @Component({
   selector: 'lesson',
   templateUrl: './lesson.component.html',
   styleUrls: ['./lesson.component.css'],
 })
-export class LessonComponent {}
+export class LessonComponent implements OnInit {
+  @Input() lesson!: Lesson;
+  videoUrl?: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    const url = `https://www.youtube.com/embed/${this.lesson.videoId}`;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
