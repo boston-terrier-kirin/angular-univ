1c5d22924114de40ba31a711c478beac90092cb2
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-25 17:04:10 +0900

    reactive-angular/Section6-37 comnbineLatestはcourse$とlessons$の初回レスポンスを待ってしまうので、 初回レスポンスとしてnullをセットする。

--- reactive-angular/client/src/app/course/course.component.ts
+++ reactive-angular/client/src/app/course/course.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest, Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, startWith, tap } from 'rxjs/operators';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
 
 interface CourseData {
-  course: Course;
+  course: Course | null;
   lessons: Lesson[];
 }
 
@@ -27,10 +27,21 @@ export class CourseComponent implements OnInit {
   ngOnInit() {
     const courseId = this.route.snapshot.paramMap.get('courseId');
     if (courseId) {
-      const course$ = this.coursesService.loadCourseById(courseId);
-      const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+      const course$ = this.coursesService.loadCourseById(courseId).pipe(
+        // comnbineLatestはcourse$とlessons$の初回レスポンスを待ってしまうので、
+        // 初回レスポンスとしてnullをセットする。
+        startWith(null)
+      );
+
+      const lessons$ = this.coursesService.loadAllCourseLessons(courseId).pipe(
+        // comnbineLatestはcourse$とlessons$の初回レスポンスを待ってしまうので、
+        // 初回レスポンスとして[]をセットする。
+        startWith([])
+      );
 
       this.data$ = combineLatest([course$, lessons$]).pipe(
+        // combineLatestで2つを束ねているが、combineLatestは両方の最新レスポンスを待ってしまう。
+        // startWithで初期値を持たせることで解決する。
         map(([course, lessons]) => {
           return {
             course,
@@ -39,6 +50,13 @@ export class CourseComponent implements OnInit {
         }),
         tap((value) => console.log(value))
       );
+
+      //・初回はすぐに、初期値が返る
+      //  {course: null, lessons: Array(0)}
+      //・次に、couserが返る
+      //  {course: {…}, lessons: Array(0)}
+      //・次に、lessonsが返る
+      //  {course: {…}, lessons: Array(11)}
     }
   }
 }
