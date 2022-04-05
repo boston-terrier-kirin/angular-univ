a6f0e3af60453cd4f90eef075c764179f1bcedd2
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-05 15:17:54 +0900

    angular_material_in_depth/Section2-13 radio radio自体の高さが20.3なので、margin-top/bottom に18.5を足して、20.3+18.5+18.5=57.3。 これで form-fieldの高さ57.31とほぼ同じになる。

--- angular_material_in_depth/client/src/app/app.module.ts
+++ angular_material_in_depth/client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,16 +13,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+import { HomeComponent } from './home/home.component';
 import { MatCardModule } from '@angular/material/card';
 import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 import { CreateCourseComponent } from './create-course/create-course.component';
 import { CreateCourseStepOneComponent } from './create-course/create-course-step-one/create-course-step-one.component';
 import { CreateCourseStepTwoComponent } from './create-course/create-course-step-two/create-course-step-two.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -46,6 +51,10 @@ import { MatStepperModule } from '@angular/material/stepper';
     MatTabsModule,
     MatCardModule,
     MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
