3ae88168cf0ea2d3b8b386caf2ed4549d60e2c8c
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-07 18:38:40 +0900

    angular-forms/Section3-25 steph型のinputの問題点 ①mat-form-fieldをinputにすると、長さが変わる。原因不明。親子関係でスタイルが変わる。 ②inputだけにすると、mat-form-fieldはmatInputが必要だといわれる。

--- angular-forms/client/src/app/login-reactive/login-reactive.component.ts
+++ angular-forms/client/src/app/login-reactive/login-reactive.component.ts
@@ -7,18 +7,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login-reactive.component.css'],
 })
 export class LoginReactiveComponent implements OnInit {
-  form = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.email],
-      // updatedOn: 'blur' を使うと、不正値 -> 有効値 に訂正した場合に、blurするまでエラーが消えないデメリットあり。
-      // updateOn: 'blur',
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(8)],
-      // updatedOn: 'blur' を使うと、不正値 -> 有効値 に訂正した場合に、blurするまでエラーが消えないデメリットあり。
-      // updateOn: 'blur',
-    }),
-  });
+  controls = this.createControls();
+  form = new FormGroup(this.controls);
 
   constructor() {}
 
@@ -26,5 +16,44 @@ export class LoginReactiveComponent implements OnInit {
 
   login() {
     console.log(this.form.value);
+  }
+
+  createControls() {
+    const email = new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      // updatedOn: 'blur' を使うと、不正値 -> 有効値 に訂正した場合に、blurするまでエラーが消えないデメリットあり。
+      // updateOn: 'blur',
+    });
+
+    const password = new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)],
+      // updatedOn: 'blur' を使うと、不正値 -> 有効値 に訂正した場合に、blurするまでエラーが消えないデメリットあり。
+      // updateOn: 'blur',
+    });
+
+    return {
+      email,
+      password,
+    };
+  }
+
+  hasErrors(name: string) {
+    const { dirty, touched, errors } = this.form.controls[name];
+    console.log(errors);
+    return dirty && touched && errors;
+  }
+
+  getErrors(name: string) {
+    const { dirty, touched, errors } = this.form.controls[name];
+    if (dirty && touched && errors) {
+      if (errors['required']) {
+        return 'Value is required.';
+      }
+      if (errors['email']) {
+        return 'Invalid format.';
+      }
+      return null;
+    }
+    return null;
   }
 }
