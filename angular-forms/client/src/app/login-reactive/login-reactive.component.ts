34986aa11f9fbe5461d470a88fd0124e17b89610
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-07 17:39:44 +0900

    angular-forms/Section3-24 updatedOn: 'blur' を使うと、不正値 -> 有効値 に訂正した場合に、blurするまでエラーが消えないデメリットあり。

--- angular-forms/client/src/app/login-reactive/login-reactive.component.ts
+++ angular-forms/client/src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-reactive.component.css'],
 })
 export class LoginReactiveComponent implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      // updateOn: 'blur',
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)],
+      updateOn: 'blur',
+    }),
+  });
+
   constructor() {}
 
   ngOnInit() {}
+
+  login() {
+    console.log(this.form.value);
+  }
 }
