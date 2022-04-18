2fa3e3db3bf311bfad400e1e539826922fb451a9
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-19 07:34:31 +0900

    angular-router/Section2 準備

---
+++ angular-router/client/src/app/courses/course/course.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css'],
+})
+export class CourseComponent implements OnInit {
+  course: Course | null = null;
+
+  couponCode: string = '';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
